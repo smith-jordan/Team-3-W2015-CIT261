bdef0cfabdb3a772fa4fa9d9929b4899ec3b8dfc
sarawr <mer12014@byui.edu>
2015-03-30 20:22:06 -0700

    update 03/30 - correct index, deleted unneeded pages

--- shopping-list/index.php
+++ shopping-list/index.php
@@ -4,40 +4,30 @@
         <title>Shopping List</title>
         <link href="stylesheet.css" type="text/css" rel="stylesheet" />
         
+        <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script src="general.js"></script>
     </head>
-    <body onload="loadColor()">
+    <body onload="doShowAll(); loadColor()">
+        <div id='wrapper'>
         <header class="themecolor" id="header">
+            <a href="index.php" id="home" class="button">Home</a>
             <h1 class="animated rubberBand">Shopping List</h1>
         </header>
         <main>
-            <table>
-                <tr>
-                    <td>List 1</td>
-                    <td>></td>
-                </tr>
-                <tr>
-                    <td>List 2</td>
-                    <td>></td>
-                </tr>
-                <tr>
-                    <td>List 3</td>
-                    <td>></td>
-                </tr>
-            </table>
+            <table id="items_table"></table>
             <div class="popup">
                 
             </div>
-            <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
-            <script src="general.js"></script>
         </main>
         <footer class="themecolor">
             <nav>
                 <ul>
-                    <li id="add"><a href="add-list" class="button">Add</a></li>
+                    <li id="add"><a href="add-item" class="button">Add</a></li>
                     <li id="options"><a href="options" class="button">Options</a></li>
-                    <li id="delete"><a href="delete-list" class="button">Delete</a></li>
+                    <li id="delete"><a href="delete-item" class="button">Delete</a></li>
                 </ul>
             </nav>
         </footer>
+        </div>
     </body>
 </html>
